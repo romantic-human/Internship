76fc29157b77678a9f523a84862d25f43e97d957
romantic-human <3401402051@qq.com>
2026-06-05 14:23:28 +0800

    fix(user): UserSerializer 更新时 password 不再必填

--- Internship-core/apps/user/serializers.py
+++ Internship-core/apps/user/serializers.py
@@ -12,6 +12,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+        extra_kwargs = {
+            "password": {"write_only": True, "required": False},
+        }
 
 
 class UserListSerializer(serializers.ModelSerializer):
